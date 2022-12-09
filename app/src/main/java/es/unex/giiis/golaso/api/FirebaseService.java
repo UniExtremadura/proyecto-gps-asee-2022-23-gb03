ae318981a9045994e28e38977447c7c7646ae815
seblazque <seblazque@alumnos.unex.es>
2022-12-09 19:06:06 +0100

    PGB03-169 IT5 - IMP - UC11 - Implement Components - DJ

--- app/src/main/java/es/unex/giiis/golaso/api/FirebaseService.java
+++ app/src/main/java/es/unex/giiis/golaso/api/FirebaseService.java
@@ -1,7 +1,14 @@
 package es.unex.giiis.golaso.api;
 
+import java.util.List;
+
+import es.unex.giiis.golaso.model.Jugador;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface FirebaseService {
 
-
+    @GET("Jugadores.json")
+    Call<List<Jugador>> callJugadores();
 
 }
