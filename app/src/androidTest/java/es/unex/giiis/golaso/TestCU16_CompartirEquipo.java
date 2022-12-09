e916dec2c272521ffdf1eacbf7f7d3922399e968
danielAbad1 <114291002+danielAbad1@users.noreply.github.com>
2022-12-10 00:49:30 +0100

    PGB03-190 IT8 - IMP - UC16 - Test Components

---
+++ app/src/androidTest/java/es/unex/giiis/golaso/TestCU16_CompartirEquipo.java
@@ -0,0 +1,119 @@
+package es.unex.giiis.golaso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TestCU16_CompartirEquipo {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testCU16_CompartirEquipo() throws InterruptedException {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.edit_user),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment_activity_main),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("prueba"), closeSoftKeyboard());
+        Thread.sleep(1000);
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.edit_password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment_activity_main),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("123456879"), closeSoftKeyboard());
+        Thread.sleep(1000);
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btn_login), withText("Iniciar sesión"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment_activity_main),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton.perform(click());
+        Thread.sleep(1000);
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.nav_table), withContentDescription("Table"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0),
+                                3),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+        Thread.sleep(1000);
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.rVClas),
+                        childAtPosition(
+                                withId(R.id.wrapper),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+        Thread.sleep(1000);
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.shareTeam),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.cardview.widget.CardView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
