c7928532d68347e116bac16288702de544d05bf3
gguiberteau <93789571+gguiberteau@users.noreply.github.com>
2022-12-09 16:47:30 +0100

    PGB03-140: IT3 - IMP - UC7 - Implement Components - AS

--- app/src/main/java/es/unex/giiis/golaso/ui/perfil/PerfilFragment.java
+++ app/src/main/java/es/unex/giiis/golaso/ui/perfil/PerfilFragment.java
@@ -1,18 +1,32 @@
 package es.unex.giiis.golaso.ui.perfil;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import es.unex.giiis.golaso.R;
 import es.unex.giiis.golaso.databinding.FragmentPerfilBinding;
 
 public class PerfilFragment extends Fragment {
 
+    private Button buttonEdit;
+    private Button buttonLogout;
+    private TextView user_email;
     private FragmentPerfilBinding binding;
+    private SharedPreferences sharedPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,19 +34,34 @@ public class PerfilFragment extends Fragment {
 
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        sharedPref = getActivity().getPreferences(getContext().MODE_PRIVATE);
 
-        final TextView textView = binding.textPerfil;
-        textView.setText("Este es el PerfilnFragment");
+        user_email = root.findViewById(R.id.user_email);
+        user_email.setText(sharedPref.getString(getString(R.string.edit_user), "No user"));
+
+        buttonLogout = (Button) root.findViewById(R.id.buttonLogout);
+        buttonEdit = (Button) root.findViewById(R.id.buttonEdit);
+
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, result) -> {
+            String email = result.getString("email");
+            user_email.setText(email);
+        });
+
+        /*buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.action_nav_profile_to_nav_login);
+            }
+        });*/
+
+        /*buttonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.action_nav_profile_to_nav_modificar_perfil);
+            }
+        });*/
 
         return root;
 
     }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
 }
-
