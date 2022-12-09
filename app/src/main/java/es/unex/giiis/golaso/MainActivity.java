f140f4beee2c6fa76bbf866bef56780359dcd916
seblazque <seblazque@alumnos.unex.es>
2022-12-09 13:04:35 +0100

    PGB03-51 IT2 - IMP - UC1 - IMPLEMENT COMPONENTS

--- app/src/main/java/es/unex/giiis/golaso/MainActivity.java
+++ app/src/main/java/es/unex/giiis/golaso/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
 import es.unex.giiis.golaso.databinding.ActivityMainBinding;
 
@@ -30,6 +31,20 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+
+            if (destination.getId() == R.id.nav_login) {
+
+                binding.navView.setVisibility(View.GONE);
+
+            } else {
+
+                binding.navView.setVisibility(View.VISIBLE);
+
+            }
+        });
+
 
     }
 
